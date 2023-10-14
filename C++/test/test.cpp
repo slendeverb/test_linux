f5f23f41cf4782aa043a13edb9d0ecc4b4a7b81b
stwings <571641990@qq.com>
2023-10-14 16:33:49 +0800

    update

---
+++ C++/test/test.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve()
+{
+    
+}
+
+signed main()
+{
+    // ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+    int T=1;
+    cin>>T;
+    while(T--)
+    {
+        solve();
+    }
+    return 0;
+}

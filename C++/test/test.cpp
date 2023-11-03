891b0e8e7ebfe6dcf2a5963c0e99a4004d7eaa1c
stwings <571641990@qq.com>
2023-11-03 21:12:28 +0800

    update

--- C++/test/test.cpp
+++ C++/test/test.cpp
@@ -6,12 +6,13 @@ using namespace std;
 
 void solve()
 {
-    
+    cout<<"hello world"<<endl;
 }
 
 signed main()
 {
     // ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+    freopen("in.txt","r",stdin);
     int T=1;
     cin>>T;
     while(T--)

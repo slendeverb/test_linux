3a331e06faaf9442ddf32877067bb83b11cab29f
stwings <571641990@qq.com>
2024-07-08 09:27:37 +0800

    update

---
+++ C#/test/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace test
+{
+    class Program
+    {
+        public static void Main(string[] args){
+            Console.WriteLine("hello world");
+            var str=Console.ReadLine();
+            Console.WriteLine(str);
+        }
+    }
+}

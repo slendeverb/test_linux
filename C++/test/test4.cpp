c41021d55890887eaba25d935721871e6e9854f5
stwings <571641990@qq.com>
2024-08-06 20:14:25 +0800

    update

--- C++/test/test4.cpp
+++ C++/test/test4.cpp
@@ -1,9 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
-void solve(){
-    
+void solve() {
+    size_t size = static_cast<size_t>(-1);
+    int *buffer=nullptr;
+    try{
+        buffer=new int[size/4];
+    }catch(const std::bad_alloc& e){
+        std::cout<<e.what()<<" "<<__func__<<" "<<__LINE__<<"\n";
+    }catch(const std::exception& e){
+        std::cout<<e.what()<<" "<<__func__<<" "<<__LINE__<<"\n";
+    }
+    if(buffer!=nullptr){
+        std::cout<<static_cast<void*>(buffer)<<"\n";
+        delete buffer;
+        buffer=nullptr;
+    }
 }
 
-int main(){
+int main() {
     solve();
 }
